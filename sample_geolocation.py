928b48a9d2e6da70a0265a84a866cdd8df0dfc25
Jean-Pierre Cuvelliez <jpcuvelliez@gmail.com>
2025-03-11 23:14:38 +0100

    .

--- sample_geolocation.py
+++ sample_geolocation.py
@@ -6,8 +6,28 @@ if __name__ == '__main__':
     }, headers={
         'Authorization': 'Bearer test'
     })
-    if res.ok:
-        data = res.json()
-        print(data)
-    else:
-        print(res.status_code)
+    if not res.ok:
+        SystemError(1)
+    data = res.json()
+    print(data)
+    originPlaceId = data['placeId']
+    # placeId: ChIJB75Std3Ew0cRYG39Y4jeL7I
+    res = requests.post('https://www.digilearn.be/geolocation/geocode', json={
+        'address': 'Bd de la Plaine 2, Ixelles'
+    }, headers={
+        'Authorization': 'Bearer test'
+    })
+    if not res.ok:
+        SystemError(1)
+    data = res.json()
+    print(data)
+    # placeId: ChIJiT0fccnEw0cR3TYDFfl-9gE
+    destinationPlaceId = data['placeId']
+    res = requests.get(f'https://www.digilearn.be/geolocation/route/{originPlaceId}/{destinationPlaceId}')
+    if not res.ok:
+        SystemError(1)
+    data = res.json()
+    print(data)
+    print(f"ULB to VUB is about {data['distance_in_m'] / 1000} km and {data['duration_in_s'] // 60} minutes by car")
+
+
